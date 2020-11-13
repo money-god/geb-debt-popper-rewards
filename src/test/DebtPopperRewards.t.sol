eaac46dd03a3658f7f130591b7f8484a03fcaca7
stefanionescu <stefanionescu@protonmail.com>
2020-11-13 02:24:04 +0000

    Test framework

--- src/test/DebtPopperRewards.t.sol
+++ src/test/DebtPopperRewards.t.sol
@@ -1,10 +1,14 @@
 pragma solidity ^0.6.7;
 
 import "ds-test/test.sol";
+import "ds-token/token.sol";
 
 import "./mock/MockTreasury.sol";
 import "./DebtPopperRewards.sol";
 
+abstract contract Hevm {
+    function warp(uint256) virtual public;
+}
 contract AccountingEngine {
     mapping (uint256 => address) public debtPoppers;
 
@@ -14,11 +18,26 @@ contract AccountingEngine {
 }
 
 contract DebtPopperRewardsTest is DSTest {
+    Hevm hevm;
+
     DebtPopperRewards popperRewards;
     AccountingEngine accountingEngine;
     MockTreasury treasury;
 
+    DSToken systemCoin;
+
+    uint256 popReward   = 5E18;
+    uint256 coinsToMint = 1E40;
+
+    uint RAY = 10 ** 27;
+    uint WAD = 10 ** 18;
+
     function setUp() public {
+        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+        hevm.warp(604411200);
+
+        
+
         popperRewards = new DebtPopperRewards();
     }
 
