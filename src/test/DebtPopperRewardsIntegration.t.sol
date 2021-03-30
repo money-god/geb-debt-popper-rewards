1b152f557a1b54bd08ed337ea9720ada4a637375
fabiohild <fabiohildebrand@gmail.com>
2021-03-29 22:19:24 -0300

    WIP integration tests

--- src/test/DebtPopperRewardsIntegration.t.sol
+++ src/test/DebtPopperRewardsIntegration.t.sol
@@ -1,37 +1,28 @@
 pragma solidity 0.6.7;
 
-import "ds-test/test.sol";
-import "ds-token/token.sol";
-
-import "./mock/MockTreasury.sol";
+import {Feed, GebDeployTestBase, EnglishCollateralAuctionHouse} from "geb-deploy/test/GebDeploy.t.base.sol";
 import "../DebtPopperRewards.sol";
 
-abstract contract Hevm {
-    function warp(uint256) virtual public;
+abstract contract WethLike {
+    function balanceOf(address) virtual public view returns (uint);
+    function approve(address, uint) virtual public;
+    function transfer(address, uint) virtual public;
+    function transferFrom(address, address, uint) virtual public;
+    function deposit() virtual public payable;
+    function withdraw(uint) virtual public;
 }
-contract AccountingEngine {
-    mapping (uint256 => address) public debtPoppers;
 
-    function popDebt(uint256 timestamp) external {
-        debtPoppers[timestamp] = msg.sender;
-    }
-}
 contract Attacker {
     function doGetRewardForPop(address popperRewards, uint256 slotTimestamp, address feeReceiver) public {
         DebtPopperRewards(popperRewards).getRewardForPop(slotTimestamp, feeReceiver);
     }
 }
 
-contract DebtPopperRewardsTest is DSTest {
-    Hevm hevm;
+contract DebtPopperRewardsIntegrationTest is GebDeployTestBase {
 
     Attacker attacker;
 
     DebtPopperRewards popperRewards;
-    AccountingEngine accountingEngine;
-    MockTreasury treasury;
-
-    DSToken systemCoin;
 
     uint256 fixedReward         = 5E18;
     uint256 coinsToMint       = 1E40;
@@ -40,18 +31,19 @@ contract DebtPopperRewardsTest is DSTest {
     uint256 maxPeriodRewards  = 50E18;
 
     uint RAY = 10 ** 27;
-    uint WAD = 10 ** 18;
 
-    function setUp() public {
-        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
-        hevm.warp(604411200);
+    function addAuthorization(address dst, address who) public {
+        DebtPopperRewards(dst).addAuthorization(who);
+    }
 
-        systemCoin       = new DSToken("RAI", "RAI");
-        accountingEngine = new AccountingEngine();
-        treasury         = new MockTreasury(address(systemCoin));
+    function setUp() override public {
+        super.setUp();
+
+        deployIndexWithCreatorPermissions(bytes32("ENGLISH"));
+
         popperRewards    = new DebtPopperRewards(
             address(accountingEngine),
-            address(treasury),
+            address(stabilityFeeTreasury),
             now + 2 weeks,
             interPeriodDelay,
             rewardTimeline,
@@ -61,16 +53,39 @@ contract DebtPopperRewardsTest is DSTest {
         );
         attacker         = new Attacker();
 
-        systemCoin.mint(address(treasury), coinsToMint);
+        // generating coin to feed StabilityFeeTreasury
+        WethLike(address(ethJoin.collateral())).deposit{value: 100 ether}();
+        WethLike(address(ethJoin.collateral())).approve(address(ethJoin), 100 ether);
+        ethJoin.join(address(this), 100 ether);
+        safeEngine.modifySAFECollateralization(
+            "ETH",
+            address(this),
+            address(this),
+            address(this),
+            100 ether,
+            1000 ether
+        );
+        safeEngine.approveSAFEModification(address(coinJoin));
+        coinJoin.exit(address(stabilityFeeTreasury), 1000 ether);
 
-        treasury.setTotalAllowance(address(popperRewards), uint(-1));
-        treasury.setPerBlockAllowance(address(popperRewards), fixedReward * 10E27);
+        // auth in stabilityFeeTreasury
+        address      usr = address(this);
+        bytes32      tag;  assembly { tag := extcodehash(usr) }
+        bytes memory fax = abi.encodeWithSignature("addAuthorization(address,address)", address(stabilityFeeTreasury), address(this));
+        uint         eta = now;
+
+        pause.scheduleTransaction(usr, tag, fax, eta);
+        pause.executeTransaction(usr, tag, fax, eta);
+
+        // setting allowances
+        stabilityFeeTreasury.setTotalAllowance(address(popperRewards), uint(-1));
+        stabilityFeeTreasury.setPerBlockAllowance(address(popperRewards), fixedReward * 10E27);
     }
 
     function test_setup() public {
         assertEq(popperRewards.authorizedAccounts(address(this)), 1);
         assertTrue(address(popperRewards.accountingEngine()) == address(accountingEngine));
-        assertTrue(address(popperRewards.treasury()) == address(treasury));
+        assertTrue(address(popperRewards.treasury()) == address(stabilityFeeTreasury));
         assertEq(popperRewards.rewardPeriodStart(), now + 2 weeks);
         assertEq(popperRewards.interPeriodDelay(), interPeriodDelay);
         assertEq(popperRewards.rewardTimeline(), rewardTimeline);
@@ -82,109 +97,114 @@ contract DebtPopperRewardsTest is DSTest {
         hevm.warp(now + 2 weeks + 1);
         uint256 newPeriodStart = now + 5;
         uint256 popTime = now;
-        accountingEngine.popDebt(popTime);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(popTime);
         popperRewards.modifyParameters("rewardPeriodStart", newPeriodStart);
         hevm.warp(now + 1);
         popperRewards.getRewardForPop(popTime, address(0));
     }
     function testFail_slot_from_future() public {
         hevm.warp(now + 2 weeks + 1);
-        accountingEngine.popDebt(now + 5);
-        popperRewards.getRewardForPop(now + 5, address(0));
+        hevm.warp(now + 5);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(now);
+        popperRewards.getRewardForPop(now, address(0));
     }
     function testFail_get_reward_after_timeline_passed() public {
         hevm.warp(now + 2 weeks + 1);
         uint slotTime = now;
-        accountingEngine.popDebt(slotTime);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(slotTime);
         hevm.warp(now + popperRewards.rewardTimeline() + 1);
         popperRewards.getRewardForPop(slotTime, address(0));
     }
     function testFail_get_reward_non_popper() public {
         hevm.warp(now + 2 weeks + 1);
         uint slotTime = now;
-        accountingEngine.popDebt(slotTime);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(slotTime);
         hevm.warp(now + 1);
         attacker.doGetRewardForPop(address(popperRewards), slotTime, address(0));
     }
     function testFail_get_twice_reward_same_slot() public {
         hevm.warp(now + 2 weeks + 1);
         uint slotTime = now;
-        accountingEngine.popDebt(slotTime);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(slotTime);
         hevm.warp(now + 1);
         popperRewards.getRewardForPop(slotTime, address(0x123));
         hevm.warp(now + 1);
         popperRewards.getRewardForPop(slotTime, address(0x123));
-    }
-    function testFail_get_reward_above_max_period_rewards() public {
-        popperRewards.modifyParameters("maxPeriodRewards", fixedReward);
-        hevm.warp(now + 2 weeks + 1);
-        uint slotTime = now;
-        accountingEngine.popDebt(slotTime);
-        accountingEngine.popDebt(slotTime + 15);
-        hevm.warp(now + 1);
-        popperRewards.getRewardForPop(slotTime, address(0x123));
-        popperRewards.getRewardForPop(slotTime + 15, address(0x123));
     }
     function testFail_get_reward_when_total_allowance_zero() public {
-        treasury.setTotalAllowance(address(popperRewards), 0);
+        stabilityFeeTreasury.setTotalAllowance(address(popperRewards), 0);
         hevm.warp(now + 2 weeks + 1);
         uint slotTime = now;
-        accountingEngine.popDebt(slotTime);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(slotTime);
         hevm.warp(now + 1);
         popperRewards.getRewardForPop(slotTime, address(0x123));
     }
     function testFail_get_reward_per_block_allowance_zero() public {
-        treasury.setPerBlockAllowance(address(popperRewards), 0);
+        stabilityFeeTreasury.setPerBlockAllowance(address(popperRewards), 0);
         hevm.warp(now + 2 weeks + 1);
         uint slotTime = now;
-        accountingEngine.popDebt(slotTime);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(slotTime);
         hevm.warp(now + 1);
         popperRewards.getRewardForPop(slotTime, address(0x123));
     }
     function test_getRewardForPop() public {
         hevm.warp(now + 2 weeks + 1);
         uint slotTime = now;
-        accountingEngine.popDebt(slotTime);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(slotTime);
         hevm.warp(now + 1);
         popperRewards.getRewardForPop(slotTime, address(0x123));
 
-        assertEq(systemCoin.balanceOf(address(0x123)), fixedReward);
+        assertEq(safeEngine.coinBalance(address(0x123)), fixedReward * 10**27);
         assertTrue(popperRewards.rewardedPop(slotTime));
         assertEq(popperRewards.rewardsPerPeriod(popperRewards.rewardPeriodStart()), fixedReward);
     }
 
     function test_getRewardForPop_gas() public {
         hevm.warp(now + 2 weeks + 1);
-        accountingEngine.popDebt(now);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(now);
         hevm.warp(now + 1);
         popperRewards.getRewardForPop(now - 1, address(this));
     }
 
     function test_getRewardForPop_gas_multiple() public {
         hevm.warp(now + 2 weeks + 1);
-        accountingEngine.popDebt(now);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(now);
         hevm.warp(now + 1);
         uint gas;
         for (uint i = 0; i < 10; i++) {
             gas = gasleft();
             popperRewards.getRewardForPop(now - 1, address(this));
             emit log_named_uint("gas", gas -gasleft());
-            accountingEngine.popDebt(now);
+            accountingEngine.pushDebtToQueue(100 ether);
+            accountingEngine.popDebtFromQueue(now);
             hevm.warp(now + 1);
         }
     }
-    function test_getRewardForPop_after_rewardPeriodStart_updated() public {
+    function test_getRewardForPop_after_rewardPeriodStart_updated2() public {
         popperRewards.modifyParameters("maxPeriodRewards", fixedReward);
         hevm.warp(now + 2 weeks + 1);
         uint slotTime = now;
-        accountingEngine.popDebt(slotTime);
-        accountingEngine.popDebt(slotTime + 15);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(slotTime);
+        hevm.warp(now + 15);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(slotTime + 15);
         hevm.warp(now + 1);
         popperRewards.getRewardForPop(slotTime, address(0x123));
         hevm.warp(popperRewards.rewardPeriodStart());
         popperRewards.getRewardForPop(slotTime + 15, address(0x123));
 
-        assertEq(systemCoin.balanceOf(address(0x123)), fixedReward * 2);
+        assertEq(safeEngine.coinBalance(address(0x123)), fixedReward * 2 * 10**27);
         assertTrue(popperRewards.rewardedPop(slotTime));
         assertTrue(popperRewards.rewardedPop(slotTime + 15));
         assertEq(popperRewards.rewardsPerPeriod(popperRewards.rewardPeriodStart()), 0);
@@ -196,7 +216,8 @@ contract DebtPopperRewardsTest is DSTest {
 
         hevm.warp(now + 2 weeks + 1);
         uint slotTime = now;
-        accountingEngine.popDebt(slotTime);
+        accountingEngine.pushDebtToQueue(100 ether);
+        accountingEngine.popDebtFromQueue(slotTime);
 
         hevm.warp(now + 1);
         popperRewards.getRewardForPop(slotTime, address(0x123));
